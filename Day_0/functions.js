56d7a2ecf660efffdac3a2d8f74b14c6e9f6985e
SAI PRASAD <sp12961369@gmail.com>
2025-08-05 00:48:30 +0530

    Arrow Functions

--- Day_0/functions.js
+++ Day_0/functions.js
@@ -2,10 +2,22 @@
 
 //It is a block of code that is used to perform a specific task
 
-//function declaration
+//Function Declaration
 function add(a,b){
     return a+b;
 }
 const c = add(2,3);
 console.log(c);
 
+//Function Expression
+const d = function(a,b){
+    return a+b;
+}
+const e = d(3,3);
+console.log(e);
+
+// Arrow Function
+const greet = (name)=>{
+    console.log(name);
+}
+greet("Sai");
